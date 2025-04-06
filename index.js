6815672e964f2386fda58d2b7505c9dd29e51e03
Dion Arya Pamungkas <dionarya.p@gmail.com>
2025-04-06 13:28:16 +0700

    change port

--- index.js
+++ index.js
@@ -215,7 +215,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3910;
 server.listen(PORT, () => {
   console.log(`Socket.io server running on port ${PORT}`);
 });
